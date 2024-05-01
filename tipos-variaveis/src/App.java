4a20becce96dcead82afb5307ff38a2553391377
RafaelCss <rf2014souza@gmail.com>
2024-05-01 07:16:33 -0300

    feat-01-05-2024 : conhecendo  tipos e  variaveis

--- tipos-variaveis/src/App.java
+++ tipos-variaveis/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salario = 2500.33;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        System.out.println(salario);
+        System.out.println(numeroCurto);
+        System.out.println(numeroNormal);
     }
 }
